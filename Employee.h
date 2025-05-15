1a65d3a85179bc79d7312993f2fbb949953818ee
SweetSalt1 <appleappolov@gmail.com>
2025-05-15 18:48:39 +0300

    Везде hours_in_week заменен на hours_in_day

--- Employee.h
+++ Employee.h
@@ -8,15 +8,15 @@
 class Employee {
 private:
 	std::string _name;
-	int _hours_in_week;
+	int _hours_in_day;
 	std::vector<Task*> _tasks;
 public:
-	Employee(const std::string& name, int hours_in_week);
+	Employee(const std::string& name, int hours_in_day);
 	void assign_task(Task* task);
 	void remove_task(Task* task);
 
 	const std::string& get_name() const;
-	int get_hours_in_week();
+	int get_hours_in_day();
 	const std::vector<Task*>& get_tasks() const;
 
 	bool is_available() const;
